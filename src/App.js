1b48996b6fbd3fc1eca94bc2823182225fd094db
Ivan <kryamky@yandex.ru>
2022-09-19 01:22:21 +0300

    filter in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 // import Counter from './components/Counter';
 // import CounterClass from './components/CounterClass';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import MySelect from './components/UI/MySelect/MySelect';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
-
+	console.log('render App');
 	const [posts, setPosts] = useState([
 		{ id: 1, title: 'Javascript', body: 'one' },
 		{ id: 2, title: 'Python', body: 'two' },
@@ -15,6 +16,35 @@ function App() {
 	])
 
 	const [selectedSort, setSelectedSort] = useState('');
+	const [searchQuery, setSearchQuery] = useState('');
+
+
+	// function getSortedPosts() {
+	// 	console.log('getSortedPosts');
+	// 	if(selectedSort) {
+	// 		return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+	// 	}
+	// 	return posts;
+	// }
+	// const sortedPosts = getSortedPosts();
+
+
+
+	const sortedPosts = useMemo(() => {
+		console.log('getSortedPosts');
+		if (selectedSort) {
+			return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+		}
+		return posts;
+	}, [selectedSort, posts])
+
+	const sortedAndSearchedPosts = useMemo(()=>{
+		return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+	}, [searchQuery, sortedPosts])
+	// const sortedAndSearchedPosts = sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+
+
+
 
 	const createPost = (post) => {
 		setPosts([...posts, post]);
@@ -25,7 +55,7 @@ function App() {
 	}
 	const sortPosts = (sort) => {
 		setSelectedSort(sort)
-		setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+		// setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort]))) // только сортировка
 	}
 
 
@@ -35,17 +65,23 @@ function App() {
 			<PostForm create={createPost} />
 			<div>
 				<hr style={{ margin: '15px 0' }} />
+				<MyInput
+					placeholder='Поиск'
+					value={searchQuery}
+					onChange={(e) => { setSearchQuery(e.target.value) }}
+				/>
 				<MySelect
 					defaultValue='Сортировка'
 					options={[{ value: 'title', name: 'По названию' }, { value: 'body', name: 'По описанию' }]}
 					value={selectedSort}
 					onChange={sortPosts}
-				// onChange={setSelectedSort}
+				// onChange={setSelectedSort} // только сортировка
 				/>
 			</div>
-			{posts.length
+			{sortedAndSearchedPosts.length
 				?
-				<PostList posts={posts} title={'Посты про Javascript'} remove={removePost} />
+				<PostList posts={sortedAndSearchedPosts} title={'Посты про Javascript'} remove={removePost} />
+				// <PostList posts={posts} title={'Посты про Javascript'} remove={removePost} /> // только сортировка
 				:
 				<h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
 			}
